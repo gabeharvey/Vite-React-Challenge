1e56315e5ce58507c87be0f45a19017321dfcee9
Gabe Harvey <harvey7@gmail.com>
2024-08-21 11:04:02 -0500

    Navbar constructed

---
+++ components/Navbar.jsx
@@ -0,0 +1,15 @@
+function Navbar () {
+    return (
+        <nav>
+            <div>
+                <img src='./src/assets/gabe-photo.jpg' alt='logo'></img>
+                <h3>React Website</h3>
+            </div>
+            <div>
+                <h3>Challenge 1</h3>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
