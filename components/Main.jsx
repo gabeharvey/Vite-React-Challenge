2117e0b6bcd312ee380d7e1ebc13364b7e0a380e
Gabe Harvey <harvey7@gmail.com>
2024-08-21 11:05:42 -0500

    Added main content area

---
+++ components/Main.jsx
@@ -0,0 +1,14 @@
+function Main () {
+    return (
+        <div>
+            <h1>This is my React Website</h1>
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+        </div>
+    )
+}
+
+export default Main;
