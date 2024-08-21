66e8c168026ff15915021df6254dadd805fe9b16
Gabe Harvey <harvey7@gmail.com>
2024-08-21 11:13:47 -0500

    Added styling import to Main.jsx

--- components/Main.jsx
+++ components/Main.jsx
@@ -1,8 +1,10 @@
+import "../src/App.css"
+
 function Main () {
     return (
         <div>
             <h1>This is my React Website</h1>
-            <ul>
+            <ul className="list-items">
                 <li>Item 1</li>
                 <li>Item 2</li>
                 <li>Item 3</li>
