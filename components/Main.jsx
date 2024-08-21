8b866cb193cfdab5bcb929f4142a3e0e7708c413
Gabe Harvey <harvey7@gmail.com>
2024-08-21 11:38:52 -0500

    Main content area styling updated

--- components/Main.jsx
+++ components/Main.jsx
@@ -2,7 +2,7 @@ import "../src/App.css"
 
 function Main () {
     return (
-        <div>
+        <div className="main-content">
             <h1>This is my React Website</h1>
             <ul className="list-items">
                 <li>Item 1</li>
