330e97ce5876e623c68dfd20bce9d80b0f679f0a
Gabe Harvey <harvey7@gmail.com>
2024-08-21 11:44:35 -0500

    Added footer.jsx content

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,1 +1,11 @@
-import "../src/App.css"
+import "../src/App.css";
+
+function Footer () {
+    return (
+        <div className="footer-content">
+            <h5>©2024 Harvey Production. All rights reserved.</h5>
+        </div>
+    )
+}
+
+export default Footer;
